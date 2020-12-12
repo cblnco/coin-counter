56539658abf5779b8a3337a03ab5db494b94e7f3
cblnco <jcarlos.kb@gmail.com>
2020-12-11 19:17:30 -0600

    Destructured props on CoinsLabel component arguments.

--- src/components/coinsLabel/CoinsLabel.jsx
+++ src/components/coinsLabel/CoinsLabel.jsx
@@ -3,18 +3,15 @@ import Coin from "../coin/Coin";
 import PropTypes from "prop-types";
 import "./CoinsLabel.css";
 
-const CoinsLabel = props => {
-  const { number, padStart, padStr } = props;
-  return (
-    <div>
-      <Coin id="coin_label_svg" className="App-Coin-label" alt="Coin label" />
-      <span className="App-Coin-label-symbol">&nbsp;🗙&nbsp;&nbsp;</span>
-      <span className="App-Coin-label-text">
-        {number.toString().padStart(padStart, padStr)}
-      </span>
-    </div>
-  );
-};
+const CoinsLabel = ({ number, padStart, padStr }) => (
+  <div>
+    <Coin id="coin_label_svg" className="App-Coin-label" alt="Coin label" />
+    <span className="App-Coin-label-symbol">&nbsp;🗙&nbsp;&nbsp;</span>
+    <span className="App-Coin-label-text">
+      {number.toString().padStart(padStart, padStr)}
+    </span>
+  </div>
+);
 
 CoinsLabel.propTypes = {
   padStr: PropTypes.string,
